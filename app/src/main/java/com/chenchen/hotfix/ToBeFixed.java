1038c2763339030534d6adfc89128c985289d6b1
“chenqun” <1715666965@qq.com>
2022-08-08 16:27:20 +0800

    热修复

---
+++ app/src/main/java/com/chenchen/hotfix/ToBeFixed.java
@@ -0,0 +1,15 @@
+package com.chenchen.hotfix;
+
+/**
+ * 待修复的类
+ */
+public class ToBeFixed {
+    /**
+     * 待修复的方法
+     *
+     * @return 字符串
+     */
+    public static String get() {
+        return "Hello, Initial Version";
+    }
+}
